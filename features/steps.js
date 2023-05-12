a2f8a731698abe87c80d7af591f35ebfce34881c
Edivansanto5 <edivansantos1478@gmail.com>
2023-05-12 14:03:41 -0300

    removendo console.log

--- features/steps.js
+++ features/steps.js
@@ -4,16 +4,14 @@ const Person = require('../src/shouty.js')
 const { Given, When, Then } = require('@cucumber/cucumber')
 
 Given('Lucy is located {int} metres from Sean', function (distance) {
-    // Write code here that turns the phrase above into concrete actions
-    // console.log(distance)
+   
     this.Lucy = new Person;
     this.sean = new Person;
     this.Lucy.moveTo(distance)
   })
   
   When('Sean shouts {string}', function (message) {
-    // Write code here that turns the phrase above into concrete actions
-    this.sean.shout(message)
+
     return 'pending'
   })
   
