e760c1aee50179120755922f44809cbb4a212b2e
Edivansanto5 <edivansantos1478@gmail.com>
2023-05-15 09:05:31 -0300

    trocando o nome de localizado no cenario

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,24 +1,15 @@
-const Person = require('../../src/shouty')
+
 const {Given,When,Then} = require("@cucumber/cucumber");
-const { assertThat, is } = require('hamjest')
 
-Given('Lucy is located {int} metres from Sean', function (distance) {
-    console.log(`Lucy is ${distance * 100} centimetres from Sean`)
-
-    // this.lucy = new Person;
-    // this.sean = new Person;
-    // this.lucy.moveTo(distance)
-  
+Given('Lucy is located {int} metre(s) from Sean', function (distance) {
+    console.log(`Lucy is ${distance * 100} centimetres from sean`)
 });
 
-When('Sean shouts {string}', function (message) {
-    // Write code here that turns the phrase above into concrete actions
-    this.sean.shout(message)
-    this.message = message
+When('Sean shouts "free bagels at Sean\'s"', function () {
+   
 });
 
 Then('Lucy hears Sean\'s message', function () {
-    assertThat(this.lucy.messagesHeard(), is([this.message]))
 
 });
 
